f14882c4c0ca6d93cf7969883e2b995fb5d56a23
funlee <i@funlee.cn>
2019-03-29 17:32:20 +0800

    'rematch'

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import React,{ Component } from 'react';
+import './index.scss';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    //
+    };
+  }
+  render() {
+    return(
+      <div>
+      hello world
+      </div>
+    )
+  }
+}
+
+export default App;
